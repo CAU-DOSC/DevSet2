9471f34e7e85e3206b718d9d6f388128bbd0ab2f
KooYS <tlfkthsl42@naver.com>
2018-06-10 14:30:03 +0900

    bst-구영서

--- DevSet2/src/com/DevSet2/Main.java
+++ DevSet2/src/com/DevSet2/Main.java
@@ -2,6 +2,7 @@ package com.DevSet2;
 
 import java.util.Scanner;
 
+import com.DevSet2.structure.IntSetBST;
 import com.DevSet2.structure.IntSetBins;
 
 public class Main {
@@ -10,27 +11,38 @@ public class Main {
 	    	int maxval;
 	    	int maxelems;
 	    	
-	    	System.out.println("maxval?");
-	    	Scanner _maxval=new Scanner(System.in);
-	    	maxval= _maxval.nextInt();
-	    	
-	    	System.out.println("maxelems?");
-	    	Scanner   _maxelems= new Scanner(System.in);
-	    	maxelems= _maxelems.nextInt();
-	    	
-	    	_maxval.close();
-	    	_maxelems.close();
+//	    	System.out.println("maxval?");
+//	    	Scanner _maxval=new Scanner(System.in);
+//	    	maxval= _maxval.nextInt();
+//	    	
+//	    	System.out.println("maxelems?");
+//	    	Scanner   _maxelems= new Scanner(System.in);
+//	    	maxelems= _maxelems.nextInt();
+//	    	
+//	    	_maxval.close();
+//	    	_maxelems.close();
+	    	maxval = 30;
+	    	maxelems = 15;
 	    	
 	    	System.out.println("------------------");
 	    	//IntSetBins(김은경)
-	    	IntSetBins bucket=new IntSetBins();
-	    	bucket.intSetImp(maxelems,maxval);
-	    	bucket.genSets();
+	    	//IntSetBins bucket=new IntSetBins();
+	    	//bucket.intSetImp(maxelems,maxval);
+	    	//bucket.genSets();
 	    	//IntSetArr(최현태)
 	    	
 	    	//IntSetBitVec(권영선)
 	    	
 	    	//IntSetBST(구영서)
+	    	IntSetBST bst = new IntSetBST();
+	    	bst.intSetImp(maxelems, maxval);
+	    	bst.dataSet();
+	    	int v[] = new int[15];
+	    	bst.report(v);
+	    	for(int i =0 ; i<15; i++)
+	    		System.out.println(v[i]);
+	    	System.out.println("size : "
+	    			+ "" + bst.size());
 	    	
 	    	//IntSetList(김남웅)
 	    	
--- DevSet2/src/com/DevSet2/structure/IntSetBST.java
+++ DevSet2/src/com/DevSet2/structure/IntSetBST.java
@@ -4,28 +4,93 @@ import java.util.*;
 // 구영서
 
 public class IntSetBST {
+	
+	class Node{
+		int element;
+		Node left;
+		Node right;	
+		int size;
+		public Node(int data){
+			element = data;
+			size = 0;
+			left = null;
+			right = null;
+		}
+	}
+
 	private int maxval;
 	private int maxelems;
+	private int explore_count;
+	private Node root;
 	
 	public IntSetBST(){
-
+		
 	}
 	
 	public void intSetImp(int maxelems, int maxval) {
-
+		//initialize set as empty
+		//double start=System.currentTimeMillis();
+		this.root = null;
+		this.maxval = maxval;
+		this.maxelems = maxelems;
+//		double end=System.currentTimeMillis();
+//		time[0] = (end - start)/1000.0;
+	}
+	
+	public void dataSet() {
+		Random random = new Random();
+		
+		while(size() < this.maxelems) {
+			insert(random.nextInt(this.maxval));
+		}	
 	}
 	
 	public void insert(int element) {
-		
+		Node newNode = new Node(element);
+		if(root==null){
+			root = newNode;
+			root.size++;
+			return;
+		}
+		Node cur = root;
+		Node prev_cur = null;
+		while(true){
+			prev_cur = cur;
+			if(element<cur.element){				
+				cur = cur.left;
+				if(cur==null){
+					prev_cur.left = newNode;
+					root.size++;
+					return;
+				}
+			}else{
+				cur = cur.right;
+				if(cur==null){
+					prev_cur.right = newNode;
+					root.size++;
+					return;
+				}
+			}
+		}
 	}
-	
-	
+	private void explore(int v[], Node node) {
+		if(node == null)
+			return;
+		
+		explore(v,node.left);
+		v[this.explore_count++] = node.element;
+		explore(v,node.right);
+	}
+
 	public int size() {
-		return 0;
+		return root == null ? 0 : root.size;
 	}
 	
-	public void report(int v) {
-		
+	public void report(int v[]) {
+		this.explore_count = 0;
+		explore(v,root);
 	}
+
+
  
 }
