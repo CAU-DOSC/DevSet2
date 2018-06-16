2a580516b8735a8bd579f9825a55e472d586cca5
kys <yskwon1231@gmail.com>
2018-06-17 03:30:11 +0900

    Bit Vector implemented in Vector<Boolean>.

--- DevSet2/src/com/DevSet2/structure/IntSetBitVec.java
+++ DevSet2/src/com/DevSet2/structure/IntSetBitVec.java
@@ -1,77 +1,52 @@
 package com.DevSet2.structure;
+
 import java.util.*;
 
 public class IntSetBitVec {
+	private Vector<Boolean> bitvec;
+	private int maxval, maxelems;
+	private int size;
 	
-	private static final int BITSPERWORD = 32;
-	private static final int SHIFT = 5;
-	private static final int MASK = 0x1F; // 0001 1111
-	private int n, hi, maxelems;
-	private int [] x;
-	
-	public void set(int i) {
-		x[i>>SHIFT] |= (1<<(i & MASK));
-	}
-	
-	public void clr(int i) {
-		x[i>>SHIFT] &= ~(1<<(i & MASK));
-	}
-	
-	public boolean test(int i) {
-		if ((x[i>>SHIFT] & (1<<(i & MASK))) == 1) return true;
-		return false;
-	}
-	
-	//maxval : maximum value of any element.
-	//maxelems : maximum number of elements.
-	
-	public IntSetBitVec(int maxelems, int maxval) {
-		hi = maxval;
-		x = new int [1 + (hi / BITSPERWORD)];
-		
-		for (int i = 0; i < hi; i++) {
-			clr(i);
-		}
-		
-		n = 0;
+	public IntSetBitVec(int maxval, int maxelems) {
+		bitvec = new Vector<Boolean>();
+		this.maxval = maxval;
 		this.maxelems = maxelems;
+		this.size = 0;
+		
+		for (int i = 0; i <= maxval; i++) {
+			bitvec.add(i, false);
+		}
 	}
 	
 	public void insert(int element) {
-		if(test(element)) {
+		if ((size >= maxelems) | (element > maxval)) {
 			return;
 		}
+		if(bitvec.get(element)) {
+			return;
+		} 
 		else {
-			set(element);
-			n++;
+			bitvec.remove(element);
+			bitvec.add(element, true);
+			size++;
 		}
 	}
 	
-	
 	public int size() {
-		return n;
+		return this.size;
 	}
 	
 	public void report(int [] v) {
 		int j = 0;
-		
-		for (int i = 0; i < hi; i++) {
-			if (test(i)) {
+		for(int i = 0; i <= maxval; i++) {
+			if(bitvec.get(i)) {
 				v[j++] = i;
 			}
 		}
 		
-		for (int i = 0; i < v.length; i++) {
-			System.out.println(i + " : " + v[i]);
-		}
-	}
-	
-	public void print() {
-		int [] result = new int [hi];
-		report(result);
-		//for (int i = 0; i < result.length; i++) {
-		//	System.out.println(i + " : " + result[i]);
+		//for(int i = 0; i < v.length; i++) {
+			//System.out.println("element >>> " + v[i]);
 		//}
 	}
- 
+	
 }
--- DevSet2/src/com/DevSet2/test/TestBitVec.java
+++ DevSet2/src/com/DevSet2/test/TestBitVec.java
@@ -2,13 +2,61 @@ package com.DevSet2.test;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.Test;
+import com.DevSet2.structure.IntSetBitVec;
 
 public class TestBitVec {
 
 	@Test
 	public void testBitVec() {
-		fail("Not yet implemented");
+		int maxvalue, maxelems;
+		long start, end, total_start;
+		
+		int mb = 1024 * 1024;
+		
+		Runtime runtime = Runtime.getRuntime();
+		
+		//Time elapsed
+		//Initialize
+		maxvalue = 1000000;
+		maxelems = maxvalue / 100;
+		
+		total_start = System.currentTimeMillis();
+		start = System.currentTimeMillis();
+		
+		
+		IntSetBitVec x = new IntSetBitVec(maxvalue, maxelems);
+		
+		end = System.currentTimeMillis();
+		System.out.println("Initialize : " + (end - start)/1000.0 + "sec");
+		
+		//Insert
+		start = System.currentTimeMillis();
+		
+		Random random = new Random();
+		while(x.size() < maxelems) {
+			x.insert(random.nextInt(maxvalue));
+		}
+		
+		end = System.currentTimeMillis();
+		System.out.println("Insert : " + (end - start)/1000.0 + "sec");
+		
+		//Insert
+		start = System.currentTimeMillis();
+		
+		int [] v = new int[maxelems];
+		x.report(v);
+		
+		end = System.currentTimeMillis();
+		System.out.println("Report : " + (end - start)/1000.0 + "sec");
+		
+		//Total
+		System.out.println("Total : " + (end - total_start)/1000.0 + "sec");
+		
+		//Space
+		System.out.println("Used Memory : " + (runtime.totalMemory() - runtime.freeMemory()) / mb);
 	}
 
 }
