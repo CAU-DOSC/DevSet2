e261a1a4d068be8046b1f7e8fdcc813d74d5b055
kys <yskwon1231@gmail.com>
2018-06-17 01:00:43 +0900

    BitVector Implemented by Integer Array.

--- DevSet2/src/com/DevSet2/Main.java
+++ DevSet2/src/com/DevSet2/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 import com.DevSet2.structure.IntSetBST;
 import com.DevSet2.structure.IntSetBins;
+import com.DevSet2.structure.IntSetBitVec;
 
 public class Main {
 	public static void main(String[] args) {
@@ -24,27 +25,38 @@ public class Main {
 	    	maxval = 30;
 	    	maxelems = 15;
 	    	
-	    	System.out.println("------------------");
-	    	//IntSetBins(김은경)
+	    	
+	    	//IntSetBins(源���寃�)
 	    	//IntSetBins bucket=new IntSetBins();
 	    	//bucket.intSetImp(maxelems,maxval);
 	    	//bucket.genSets();
-	    	//IntSetArr(최현태)
+	    	//IntSetArr(理쒗쁽�깭)
 	    	
-	    	//IntSetBitVec(권영선)
+	    	//IntSetBitVec(沅뚯쁺�꽑)
+	    	System.out.println("Bit Vector ------------------");
+	    	IntSetBitVec bitvec = new IntSetBitVec(64, 64);
+	    	bitvec.insert(10);
+	    	boolean bit = bitvec.test(10);
+	    	System.out.println("bit : " + bit);
 	    	
-	    	//IntSetBST(구영서)
-	    	IntSetBST bst = new IntSetBST();
-	    	bst.intSetImp(maxelems, maxval);
-	    	bst.dataSet();
-	    	int v[] = new int[15];
-	    	bst.report(v);
-	    	for(int i =0 ; i<15; i++)
-	    		System.out.println(v[i]);
-	    	System.out.println("size : "
-	    			+ "" + bst.size());
+	    	int [] result = new int [100];
+			bitvec.report(result);
 	    	
-	    	//IntSetList(김남웅)
+	    	
+	    	
+	    	//IntSetBST(援ъ쁺�꽌)
+	    	//System.out.println("------------------");
+	    	//IntSetBST bst = new IntSetBST();
+	    	//bst.intSetImp(maxelems, maxval);
+	    	//bst.dataSet();
+	    	//int v[] = new int[15];
+	    	//bst.report(v);
+	    	//for(int i =0 ; i<15; i++)
+	    	//	System.out.println(v[i]);
+	    	//System.out.println("size : "
+	    		//	+ "" + bst.size());
+	    	
+	    	//IntSetList(源��궓�썒)
 	    	
 	    	
 	    	System.out.println("------------------");
--- DevSet2/src/com/DevSet2/structure/IntSetBitVec.java
+++ DevSet2/src/com/DevSet2/structure/IntSetBitVec.java
@@ -1,31 +1,77 @@
 package com.DevSet2.structure;
 import java.util.*;
- 
-// 권영선
 
 public class IntSetBitVec {
-	private int maxval;
-	private int maxelems;
 	
-	public IntSetBitVec(){
-
+	private static final int BITSPERWORD = 32;
+	private static final int SHIFT = 5;
+	private static final int MASK = 0x1F; // 0001 1111
+	private int n, hi, maxelems;
+	private int [] x;
+	
+	public void set(int i) {
+		x[i>>SHIFT] |= (1<<(i & MASK));
 	}
 	
-	public void intSetImp(int maxelems, int maxval) {
-
+	public void clr(int i) {
+		x[i>>SHIFT] &= ~(1<<(i & MASK));
+	}
+	
+	public boolean test(int i) {
+		if ((x[i>>SHIFT] & (1<<(i & MASK))) == 1) return true;
+		return false;
+	}
+	
+	//maxval : maximum value of any element.
+	//maxelems : maximum number of elements.
+	
+	public IntSetBitVec(int maxelems, int maxval) {
+		hi = maxval;
+		x = new int [1 + (hi / BITSPERWORD)];
+		
+		for (int i = 0; i < hi; i++) {
+			clr(i);
+		}
+		
+		n = 0;
+		this.maxelems = maxelems;
 	}
 	
 	public void insert(int element) {
-		
+		if(test(element)) {
+			return;
+		}
+		else {
+			set(element);
+			n++;
+		}
 	}
 	
 	
 	public int size() {
-		return 0;
+		return n;
 	}
 	
-	public void report(int v) {
+	public void report(int [] v) {
+		int j = 0;
 		
+		for (int i = 0; i < hi; i++) {
+			if (test(i)) {
+				v[j++] = i;
+			}
+		}
+		
+		for (int i = 0; i < v.length; i++) {
+			System.out.println(i + " : " + v[i]);
+		}
+	}
+	
+	public void print() {
+		int [] result = new int [hi];
+		report(result);
+		//for (int i = 0; i < result.length; i++) {
+		//	System.out.println(i + " : " + result[i]);
+		//}
 	}
  
 }
