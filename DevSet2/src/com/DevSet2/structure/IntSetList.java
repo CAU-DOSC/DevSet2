5060acde9a3bb1c17d033bbf613b076916eeac08
namwooong <nwkim1@naver.com>
2018-06-17 19:48:17 +0900

    List 구현
    List 구현

--- DevSet2/src/com/DevSet2/structure/IntSetList.java
+++ DevSet2/src/com/DevSet2/structure/IntSetList.java
@@ -6,26 +6,77 @@ import java.util.*;
 public class IntSetList {
 	private int maxval;
 	private int maxelems;
+	class Node{
+		int element;
+		Node next;	
+	
+		public Node(int data){
+			element = data;
+			next = null;
+			
+		}
+	}
+	private Node head;
+	private int size;
+
+
 	
 	public IntSetList(){
-
+		this.head=new Node(-1);
+		size=0;
 	}
 	
 	public void intSetImp(int maxelems, int maxval) {
-
+		
+		this.maxval=maxval;
+		this.maxelems=maxelems;
+		Random random = new Random();
+		while(size() < this.maxelems) {
+			insert(random.nextInt(this.maxval));
+		}	
 	}
 	
 	public void insert(int element) {
+		//오름차순으로 입력
+		this.size=this.size+1;
+		Node temp=new Node(element);
+		Node now=this.head;
+		
+		while(now.next!=null)
+		{
+
+			if(now.next.element<=temp.element)
+			{
+				now=now.next;
+			}    
+			else
+			{
+				temp.next=now.next;
+				now.next=temp;
+				return;
+			}
+
+		}
+
+		now.next=temp;
+		return;
 		
 	}
 	
 	
 	public int size() {
-		return 0;
+		return this.size;
 	}
 	
-	public void report(int v) {
-		
+	public void report(int[] v) {
+		Node now=this.head;
+		int index=0;
+		while(now.next!=null)
+		{
+			v[index++]=now.next.element;
+			now=now.next;
+		}
+	
 	}
  
 }
--- DevSet2/src/com/DevSet2/test/TestList.java
+++ DevSet2/src/com/DevSet2/test/TestList.java
@@ -2,13 +2,74 @@ package com.DevSet2.test;
 
 import static org.junit.Assert.*;
 
+import java.util.Scanner;
+
 import org.junit.Test;
+
+import com.DevSet2.structure.IntSetList;
 
 public class TestList {
 
 	@Test
 	public void testList() {
-		fail("Not yet implemented");
-	}
+		int maxvalue;
+		int maxelems;
+		double start,total_start;
+    	double end;
+		String time_exp[]={"initialize","insert","report","TOTAL"};
+		
+		
+		Scanner s=new Scanner(System.in);
+    	System.out.println("maxval?");
+    	maxvalue=s.nextInt();
+       
+    	System.out.println("maxelems?");
+    	Scanner s2=new Scanner(System.in);
+    	maxelems=s2.nextInt();
+		
+    	s.close();
+    	s2.close();
+    	
+    	
+    	System.gc();
+        Runtime r = Runtime.getRuntime();
+       
+    	total_start=System.currentTimeMillis();
+    	
+    	//check time complexity
+    	System.out.println("--------TIME COMPLEXITY----------");
+    	
+    	//initialize
+    	start=System.currentTimeMillis();
+    	IntSetList lst = new IntSetList();
+    	end=System.currentTimeMillis();
+    	System.out.println(time_exp[0]+": "+(end - start)/1000.0+"s");
+    	
+    	//insert
+    	start=System.currentTimeMillis();
+    	long firstMemorySize = r.freeMemory();
+    	lst.intSetImp(maxelems, maxvalue);
+    	long lastMemorySize = r.freeMemory();
+    	end=System.currentTimeMillis();
+    	System.out.println(time_exp[1]+": "+(end - start)/1000.0+"s");
+    	
+    	//report
+    	start=System.currentTimeMillis();
+    	int v[] = new int[maxelems];
+    	lst.report(v);
+    	end=System.currentTimeMillis();
+    	System.out.println(time_exp[2]+": "+(end - start)/1000.0+"s");
+    	
+    	//TOTAL
+    	end=System.currentTimeMillis();
+    	System.out.println(time_exp[3]+": "+(end - total_start)/1000.0+"s");
+		
+    	
+        System.out.println("가용한 힙 메모리" + firstMemorySize  + "bytes");
+        System.out.println("객체 생성 후 가용한 힙 메모리" + lastMemorySize  + "bytes");
+        System.out.println("객체 하나의 메모리 크기는 " + (firstMemorySize - lastMemorySize)/maxelems + "bytes");
 
+	}	
 }
+
+
