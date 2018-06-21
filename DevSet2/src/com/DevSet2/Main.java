32f1252d22820d9239419b765430a5f96a4cda82
namwooong <nwkim1@naver.com>
2018-06-21 15:37:02 +0900

    최신화
    최신화

--- DevSet2/src/com/DevSet2/Main.java
+++ DevSet2/src/com/DevSet2/Main.java
@@ -3,6 +3,7 @@ package com.DevSet2;
 import java.util.Scanner;
 
 import com.DevSet2.structure.*;
+import com.DevSet2.test.TestArr;
 
 
 public class Main {
@@ -11,26 +12,43 @@ public class Main {
 	    	int maxval;
 	    	int maxelems;
 	    	
-//	    	System.out.println("maxval?");
-//	    	Scanner _maxval=new Scanner(System.in);
-//	    	maxval= _maxval.nextInt();
-//	    	
-//	    	System.out.println("maxelems?");
-//	    	Scanner   _maxelems= new Scanner(System.in);
-//	    	maxelems= _maxelems.nextInt();
-//	    	
-//	    	_maxval.close();
-//	    	_maxelems.close();
+	    	System.out.println("maxval?");
+	    	Scanner _maxval=new Scanner(System.in);
+	    	maxval= _maxval.nextInt();
+
+	    	System.out.println("maxelems?");
+	    	Scanner   _maxelems= new Scanner(System.in);
+	    	maxelems= _maxelems.nextInt();
+
+	    	_maxval.close();
+	    	_maxelems.close();
 	    	maxval = 30;
 	    	maxelems = 15;
 	    	
-	    	/*
-	    	 
-	    	//IntSetBins()
-	    	//IntSetBins bucket=new IntSetBins();
-	    	//bucket.intSetImp(maxelems,maxval);
-	    	//bucket.genSets();
-	    	//IntSetArr()
+	    	
+
+
+	    	System.out.println("Bin Sort--------------------");
+	    	IntSetBins bucket=new IntSetBins();
+	    	bucket.intSetImp(maxelems,maxval);
+	    	bucket.genSets();
+	    	bucket.report();
+	    	//bucket.printAll();  : if you want to print sorted result
+	    	//bucket.printTime(); : if you want to print time info
+	    	//bucket.getSpace();  : if you want to know space complexity
+	    	System.out.println("---------Array---------");
+			  IntSetArr arr = new IntSetArr();
+			  TestArr arrTest = new TestArr();
+			  arr.intSetImp(maxelems,maxval);
+			  arr.arrayData();
+			  int a[] = new int[15];
+			  arr.report(a);
+			  for(int i =0 ; i<15; i++)
+				  System.out.println(a[i]);
+			  System.out.println("size : "
+				  + "" + arr.size());
+			  //arrTest.testArr();
+
 	    	
 	    	//IntSetBitVec()
 	    	System.out.println("Bit Vector ------------------");
@@ -41,11 +59,11 @@ public class Main {
 	    	
 	    	int [] result = new int [100];
 			bitvec.report(result);
+
 	    	
-	    	*/
-	    	
+	 	    	
 	    	//IntSetBST()
-	    	System.out.println("IntSetBST()");
+	    	System.out.println("---------BST---------");
 	    	IntSetBST bst = new IntSetBST();
 	    	bst.intSetImp(maxelems, maxval);
 	    	bst.dataSet();
@@ -55,21 +73,23 @@ public class Main {
 	    		System.out.println(v[i]);
 	    	System.out.println("size : "
 	    		+ "" + bst.size());
-	    	
+
+
+
 	    	//IntSetList()
-	    	System.out.println("IntSetList()");
-	    	IntSetList lst = new IntSetList();
-	    	lst.intSetImp(maxelems, maxval);
-	    	
-	    	int q[] = new int[15];
-	    	lst.report(q);
-	    	for(int i =0 ; i<15; i++)
-	    		System.out.println(q[i]);
-	    	System.out.println("size : "
-	    			+ "" + lst.size());
-	    	
-	    	
-	    	System.out.println("------------------");
+			//System.out.println("------------------");
+	    	//IntSetList lst = new IntSetList();
+	    	//lst.intSetImp(maxelems, maxval);
+	    	//lst.dataSet();
+	    	//int b[] = new int[15];
+	    	//bst.report(b);
+	    	//for(int i =0 ; i<15; i++)
+	    	//	System.out.println(b[i]);
+	    	//System.out.println("size : "
+	    	//		+ "" + lst.size());
+	    	//
+	    	//
+	    	//System.out.println("------------------");
 
 	
 	}//main END
